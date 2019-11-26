af85402382e5130edd3d956f8d8b98c1bd387f8b
bg2015 <XMG3-Rel.1>
2019-11-26 21:41:17 +0800

    SVM增加注释

--- fangWei_DMandML_inAction/chapter5_SVM/5.4.1.py
+++ fangWei_DMandML_inAction/chapter5_SVM/5.4.1.py
@@ -6,6 +6,7 @@ from sklearn import svm
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 if __name__ == "__main__":
+    # # step1 随机的方法生成训练集
     N = 50
     np.random.seed(0)
     print('训练数据集(x,y)：')
@@ -14,16 +15,26 @@ if __name__ == "__main__":
     y = 2*np.sin(x) + 0.1*np.random.randn(N)
     x = x.reshape(-1, 1)
     print ('y =\n', y)
+
+    # bg added 20191126 训练模型
+    # step2-1: 使用 高斯核函数 进行训练 
     print ('SVR - RBF')
     svr_rbf = svm.SVR(kernel='rbf', gamma=0.4, C=100)
     svr_rbf.fit(x, y)
+
+    # step2-2: 使用 线性核函数 进行训练 
     print ('SVR - Linear')
     svr_linear = svm.SVR(kernel='linear', C=100)
     svr_linear.fit(x, y)
+
+    # step2-3:使用 多项式核函数 进行训练  
     print ('SVR - Polynomial')
     svr_poly = svm.SVR(kernel='poly', degree=3, C=100)
     svr_poly.fit(x, y)
+
     print ('Fit OK.')
+
+    # step3 生成测试数据集
     x_test = np.linspace(x.min(), 1.5*x.max(), 50)
     print('测试数据集(x_test,y_test)：')
     print('x_test=\n',x_test)
@@ -31,9 +42,13 @@ if __name__ == "__main__":
     y_test = 2*np.sin(x_test) + 0.1*np.random.randn(N)
     print('y_test=\n',y_test)
     x_test=x_test.reshape(-1,1)
+
+    #  step4 使用三种核函数进行预测
     y_rbf = svr_rbf.predict(x_test)
     y_linear = svr_linear.predict(x_test)
     y_poly = svr_poly.predict(x_test)   
+
+    # 作图step5 
     plt.figure(figsize=(9, 8), facecolor='w')
     plt.plot(x_test, y_rbf, 'r-', linewidth=2, label='RBF Kernel')
     plt.plot(x_test, y_linear, 'g-', linewidth=2, label='Linear Kernel')
@@ -48,6 +63,8 @@ if __name__ == "__main__":
     plt.grid(True)
     plt.tight_layout(2)
     plt.show()
+
+    # step6 用平均绝对误差和均方误差为来评价模型的准确性
     print("高斯核函数支持向量机的平均绝对误差为:", mean_absolute_error(y_test,y_rbf))
     print("高斯核函数支持向量机的均方误差为:", mean_squared_error(y_test,y_rbf))
     print("线性核函数支持向量机的平均绝对误差为:", mean_absolute_error(y_test,y_linear))
--- fangWei_DMandML_inAction/chapter5_SVM/5.4.7.py
+++ fangWei_DMandML_inAction/chapter5_SVM/5.4.7.py
@@ -1,12 +1,15 @@
 # !/usr/bin/python
 # -*- coding:utf-8 -*-
 
+#  bg added 20191126  支持向量机回归(SVR)的例子 训练集与5.4.1.py相同
 import numpy as np
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV    # 0.17 grid_search
 import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 if __name__ == "__main__":
+
+    # step1 随机的方法生成训练集
     N = 50
     np.random.seed(0)
     x = np.sort(np.random.uniform(0, 6, N), axis=0)
@@ -15,7 +18,9 @@ if __name__ == "__main__":
     y = 2*np.sin(x) + 0.1*np.random.randn(N)
     x = x.reshape(-1, 1)
     print ('y =\n', y)
-    model = svm.SVR(kernel='rbf')
+
+    # step2:    训练模型
+    model = svm.SVR(kernel='rbf')   # bg 注意这里调用了svm的SVR方法
     c_can = np.linspace(105,107,10)
     print('c_can=',c_can)
     gamma_can = np.linspace(0.4, 0.5, 10)
@@ -24,16 +29,22 @@ if __name__ == "__main__":
     svr_rbf.fit(x, y)
     print ('最优参数：\n', svr_rbf.best_params_)
     print('测试数据集(x_test,y_test)：')
+
+    # step3 生成测试集
     x_test = np.linspace(x.min(), 1.5*x.max(), 50)
     print('x_test=\n',x_test)
     np.random.seed(0)
     y_test = 2*np.sin(x_test) + 0.1*np.random.randn(N)
     print('y_test=\n',y_test)
     x_test=x_test.reshape(-1,1)
+
+    # step4 使用训练好的模型进行预测
     y_rbf = svr_rbf.predict(x_test)
     print('高斯核的预测值:')
     print('y_rbf=\n',y_rbf)
     sp = svr_rbf.best_estimator_.support_
+
+    # step5  作图
     plt.figure(figsize=(9, 8),facecolor='w')
     plt.scatter(x[sp], y[sp], s=200, c='r', marker='*', label='Support Vectors')
     plt.plot(x_test, y_rbf, 'r-', linewidth=2, label='RBF Kernel')
@@ -45,5 +56,7 @@ if __name__ == "__main__":
     plt.ylabel('Y')
     plt.grid(True)
     plt.show()
+
+    # step6 用平均绝对误差和均方误差为来评价模型的准确性
     print("选取最优参数的高斯核函数支持向量机的平均绝对误差为:", mean_absolute_error(y_test,y_rbf))
     print("选取最优参数的高斯核函数支持向量机的均方误差为:", mean_squared_error(y_test,y_rbf))
