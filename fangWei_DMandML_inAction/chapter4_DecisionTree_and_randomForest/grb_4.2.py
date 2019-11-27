499d1bda8186923208af97f9514d98322383b598
renin.guo <renbin.guo@jci.com>
2019-11-27 17:00:32 +0800

    为决策树增加预测的代码

--- fangWei_DMandML_inAction/chapter4_DecisionTree_and_randomForest/grb_4.2.py
+++ fangWei_DMandML_inAction/chapter4_DecisionTree_and_randomForest/grb_4.2.py
@@ -5,8 +5,16 @@ from sklearn.tree.export import export_text
 iris = load_iris()
 X = iris['data']
 y = iris['target']
+print('x = ',X)
+print('y = ',y)
 # decision_tree = DecisionTreeClassifier(random_state=0, max_depth=2)
 decision_tree = DecisionTreeClassifier()
 decision_tree = decision_tree.fit(X, y)
 r = export_text(decision_tree, feature_names=iris['feature_names'])
 print(r)
+
+# bg added  用训练好的模型进行预测
+preX = [[5.9 ,3.0,  5.1, 1.8]]
+# [[1, 1,1,1]]
+rt = decision_tree.predict(preX)
+print("rt = ",rt)
